d0b2a3c9ac5dcfff02d5c46521f4056805df774e
rustikk <tunruh03@gmail.com>
2021-08-20 21:46:24 -0600

    bug fixing postgresql.. mabye

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 app = Flask(__name__)
-uri = os.getenv("DATABASE_URL", "sqlite:///data.db")
+uri = os.getenv("DATABASE_URL")
 if uri.startswith("postgres://"):
     uri = uri.replace("postrgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
